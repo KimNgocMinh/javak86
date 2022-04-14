068b562ec7efff82ddb0347f75db89c7d83cbe63
KimNgocMinh <kidmouse0000@gmail.com>
2022-04-14 20:16:20 +0700

    nop bai 2.1

---
+++ src/lesson2/Activity2_1.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Activity2_1 {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.print("enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("enter second number: ");
+        int num2 = input.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
+    }
+}
